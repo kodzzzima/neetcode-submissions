6112c9f4b130a624c766969ec9eecffea55c6dc6
Dmitry <66273662+kodzzzima@users.noreply.github.com>
2026-06-13 14:32:11 +0300

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        if (s.length != t.length) return false
+        if(s.toCharArray().sorted() == t.toCharArray().sorted()) return true
+        return false
+    }
+}
